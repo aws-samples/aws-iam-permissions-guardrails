585316eb2f9c1e9d1bdd81013573ade23cd2a61d
0xjjoyy <jjjoy@amazon.com>
2020-11-04 11:22:01 -0500

    cleanup s3 bucket tags boto exception handling

--- access-analyzer/step-functions-archive-findings/functions/context-enrichment/app.py
+++ access-analyzer/step-functions-archive-findings/functions/context-enrichment/app.py
@@ -4,15 +4,21 @@ logger.setLevel(logging.INFO)
 
 from collections import ChainMap
 import boto3
+from botocore.exceptions import ClientError
 
-def lookup_s3_tags(resource_arn):
+def lookup_s3_tags(bucket_name):
   s3_client = boto3.client("s3")
-  bucket_tags = s3_client.get_bucket_tagging(Bucket=resource_arn)["TagSet"]
+  bucket_tags=[]
+  try:
+    bucket_tags = s3_client.get_bucket_tagging(Bucket=bucket_name)["TagSet"]
 
-  for tags in bucket_tags:
-    tags[tags.pop("Key")]=tags.pop("Value")
+    for tags in bucket_tags:
+      tags[tags.pop("Key")]=tags.pop("Value")
+  except ClientError:
+    #https://github.com/boto/boto3/issues/341
+    logger.exception(f"Error retrieving tags for S3 bucket {bucket_name}")
 
-  return dict(ChainMap(*tags))
+  return dict(ChainMap(*bucket_tags))
 
 def lookup_kms_tags(resource_arn):
   kms_client = boto3.client("kms")
@@ -27,7 +33,8 @@ def lookup_tags(resource_type,resource_arn):
   if resource_type=="AWS::KMS::Key": 
     return lookup_kms_tags(resource_arn)
   elif resource_type=="AWS::S3::Bucket":
-    return lookup_s3_tags(resource_arn)
+    bucket_name=resource_arn.split(':')[-1]
+    return lookup_s3_tags(bucket_name)
   else:
     logger.info(f"{resource_type} needs to be added to lookup tags")
     return {}
@@ -49,12 +56,6 @@ def handler(event,context):
   finding_id=detail["id"]
   resource_type=detail["resourceType"]
   resource_arn=detail["resource"]
-  is_public=detail["isPublic"]
-  actions=detail["action"]
-  condition=detail["condition"]
-  principal=detail["principal"]
-
-  #logger.info(f"finding id={finding_id} analyzer arn={analyzer_arn} is public={is_public} resource type={resource_type} principal={principal} actions={actions} condition={condition}")
 
   tags=lookup_tags(resource_type,resource_arn) 
   additional_context=additional_enrichment(resource_type,resource_arn)
