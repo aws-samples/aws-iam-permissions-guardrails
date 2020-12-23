75bb5b1efcc395eb6f9b474a62a9a1f69c4e0e86
jjjoy@amazon.com <jjjoy@amazon.com>
2020-12-23 09:41:35 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="iam-permissions-guardrails",  # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Josh Joy",
     author_email="jjjoy@amazon.com",
     description="IAM Permissions Guardrails module",
